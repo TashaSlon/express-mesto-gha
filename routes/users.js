060e90edb64348d4f3e1476c21866b99a28bc454
plyusnina <plyusnina@live.ru>
2023-06-29 17:52:23 +0500

    user fixes

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const {
   getUsers, getUser, updateProfile, updateAvatar, getProfile,
 } = require('../controllers/users');
 
+router.get('/me', getProfile);
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum(),
+    userId: Joi.string().alphanum().min(20),
   }),
 }), getUser);
 router.get('/', getUsers);
-router.get('/me', getProfile);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     email: Joi.string().regex(/^[a-z0-9._%+-]+@[a-z0-9-]+.+.[a-z]{2,4}$/i),
